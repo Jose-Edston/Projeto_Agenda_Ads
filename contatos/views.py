c53a17d57601099cc5c0ab7125795e90698ee855
Jose-Edston <joseedston@gmail.com>
2023-04-27 21:05:56 -0300

    second commit

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contatos
 
 def index(request):
-    return render(request, 'pages/index.html')
+    contatos = Contatos.objects.all()
+    return render(request, 'pages/index.html', {'contatos': contatos})
+
+def search(request):
+    q = request.GET.get('search')
+    contatos = Contatos.objects.filter(nome__icontains=q) # o __icontains é para filtrar o que tem com o que foi digitado, sem isso, ele pesquisa por algo que seja igual
+    return render(request, 'pages/index.html', {'contatos': contatos})
